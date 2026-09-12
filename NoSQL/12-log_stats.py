1914ca178071dd91537bf26b6ef55ebda2d19c43
Abeer Al-Sayaari <abeer.ali.alsaiari1@gmail.com>
2026-09-12 22:53:18 +0300

    Final log stats

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,24 +1,28 @@
 #!/usr/bin/env python3
-"""
-Python script that provides some stats about Nginx logs stored in MongoDB.
-"""
+"""Script that provides stats about Nginx logs stored in MongoDB."""
 from pymongo import MongoClient
-
+from pymongo.errors import ConnectionFailure
 
 if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    nginx_collection = client.logs.nginx
+    try:
+        client = MongoClient('127.0.0.1', 27017, serverSelectionTimeoutMS=2000)
+        # اختبار الاتصال
+        client.admin.command('ping')
 
-    total_logs = nginx_collection.count_documents({})
-    print(f"{total_logs} logs")
+        nginx_collection = client.logs.nginx
+        n_logs = nginx_collection.count_documents({})
+        print(f"{n_logs} logs")
 
-    print("Methods:")
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    for method in methods:
-        count = nginx_collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {count}")
+        print("Methods:")
+        methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+        for method in methods:
+            count = nginx_collection.count_documents({"method": method})
+            print(f"\tmethod {method}: {count}")
 
-    status_checks = nginx_collection.count_documents(
-        {"method": "GET", "path": "/status"}
-    )
-    print(f"{status_checks} status check")
+        status_check = nginx_collection.count_documents(
+            {"method": "GET", "path": "/status"}
+        )
+        print(f"{status_check} status check")
+
+    except ConnectionFailure:
+        print("خطأ: قاعدة البيانات MongoDB غير شغالة على جهازك المحلي حالياً.")
