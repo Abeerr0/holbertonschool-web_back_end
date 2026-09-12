bf4d984e0e2b1dd02b251bc028b252f4c68cf6f3
Abeer Al-Sayaari <abeer.ali.alsaiari1@gmail.com>
2026-09-12 22:30:52 +0300

    Fix log stats scripts

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -7,18 +7,15 @@ if __name__ == "__main__":
     client = MongoClient('127.0.0.1', 27017)
     nginx_collection = client.logs.nginx
 
-    # 1. Total logs
     n_logs = nginx_collection.count_documents({})
     print(f"{n_logs} logs")
 
-    # 2. Methods
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
         count = nginx_collection.count_documents({"method": method})
         print(f"\tmethod {method}: {count}")
 
-    # 3. Status check
     status_check = nginx_collection.count_documents(
         {"method": "GET", "path": "/status"}
     )
